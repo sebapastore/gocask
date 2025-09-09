7c0937157fb2516f9767b790411af7e33b45dce5
sebapastore <sebastianerp@gmail.com>
2025-09-09 12:35:43 -0300

    Add in-memory database with file persistance

--- cmd/gocask/main.go
+++ cmd/gocask/main.go
@@ -1,13 +1,21 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
+	"os"
+	"strings"
 )
 
+type Database struct {
+	data map[string]string
+	path string
+}
+
 func main() {
-	var db string
-	flag.StringVar(&db, "db", "./database", "Database path")
+	var dbPath string
+	flag.StringVar(&dbPath, "db", "./database", "Database path")
 
 	flag.Usage = printUsage
 
@@ -21,6 +29,17 @@ func main() {
 
 	command := args[0]
 
+	if err := initDatabaseFile(dbPath); err != nil {
+		fmt.Println("Error initializing database file:", err)
+		os.Exit(1)
+	}
+
+	db, err := loadDatabaseFromFile(dbPath)
+	if err != nil {
+		fmt.Println("Error loading database:", err)
+		os.Exit(1)
+	}
+
 	switch command {
 	case "set":
 		if len(args) < 3 {
@@ -29,6 +48,11 @@ func main() {
 		}
 		key := args[1]
 		value := args[2]
+		db.data[key] = value
+		if err := db.Save(); err != nil {
+			fmt.Printf("Error while saving the database: %s", err)
+			os.Exit(1)
+		}
 		fmt.Printf("SET key=%s value=%s\n", key, value)
 
 	case "get":
@@ -37,12 +61,16 @@ func main() {
 			return
 		}
 		key := args[1]
-		fmt.Printf("GET key=%s\n", key)
+		value := db.data[key]
+		if value == "" {
+			fmt.Printf("There is no value for key %s\n", key)
+			os.Exit(1)
+		}
+		fmt.Printf("The value for key %s is %s\n", key, value)
 
 	default:
 		fmt.Println("Unknown command:", command)
 	}
-
 }
 
 func printUsage() {
@@ -60,4 +88,70 @@ func printUsage() {
   gocask set name Sebastian --db ./mydb
   gocask get name --db ./mydb
 	`)
+}
+
+func initDatabaseFile(dbPath string) error {
+	if _, err := os.Stat(dbPath); os.IsNotExist(err) {
+		file, err := os.Create(dbPath)
+		if err != nil {
+			return fmt.Errorf("failed to create database file: %w", err)
+		}
+		defer file.Close()
+		fmt.Println("Database created at", dbPath)
+	}
+	return nil
+}
+
+func loadDatabaseFromFile(path string) (*Database, error) {
+	db := &Database{
+		data: make(map[string]string),
+		path: path,
+	}
+
+	// Open the file
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, fmt.Errorf("failed to open database: %w", err)
+	}
+	defer file.Close()
+
+	// Read line by line
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		if line == "" {
+			continue
+		}
+		// simple format: key=value
+		parts := strings.SplitN(line, ":", 2)
+		if len(parts) != 2 {
+			continue
+		}
+		key := parts[0]
+		value := parts[1]
+		db.data[key] = value
+	}
+
+	if err := scanner.Err(); err != nil {
+		return nil, fmt.Errorf("failed to read database: %w", err)
+	}
+
+	return db, nil
+}
+
+func (db *Database) Save() error {
+	file, err := os.Create(db.path)
+	if err != nil {
+		return fmt.Errorf("failed to save database: %w", err)
+	}
+	defer file.Close()
+
+	for k, v := range db.data {
+		_, err := fmt.Fprintf(file, "%s:%s\n", k, v)
+		if err != nil {
+			return fmt.Errorf("failed to write database: %w", err)
+		}
+	}
+
+	return nil
 }
